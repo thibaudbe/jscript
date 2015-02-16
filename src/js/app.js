949da87283846b0637d42a67046f68f7546642ef
Thibaud B <thibaud.tibo@gmail.com>
2015-02-16 16:13:42 +0100

    current tab view detection

--- src/js/app.js
+++ src/js/app.js
@@ -7,6 +7,10 @@
 	var scriptInput = document.querySelector('#scriptInput');
 	var htmlInput = document.querySelector('#htmlInput');
 
+	var tab1 = document.querySelector('#tab1');
+	var tab2 = document.querySelector('#tab2');
+	var tab3 = document.querySelector('#tab3');
+
 	var library = document.querySelector('#library');
 	var sass = document.querySelector('#sass');
 
@@ -18,58 +22,75 @@
 	 */
 
 	var settings = function() {
-    return {
-      title: null,
-      description: null,
-      library: library.checked ? true : false,
-      sass: sass.checked ? true : false,
-      ressources: [],
-      style: styleInput.value,
-      script: scriptInput.value,
-      html: htmlInput.value
-    };
+		return {
+			title: null,
+			description: null,
+			library: library.checked ? true : false,
+			sass: sass.checked ? true : false,
+			ressources: [],
+			view: {
+				tab1: tab1.checked ? true: false,
+				tab2: tab2.checked ? true: false,
+				tab3: tab3.checked ? true: false
+			},
+			style: styleInput.value,
+			script: scriptInput.value,
+			html: htmlInput.value
+		};
 	};
 
 	var run = function() {
-    var output = settings();
+		var data = settings();
 
-    var resultFrame = document.querySelector('#resultFrame');
-    if (resultFrame) { resultFrame.remove() }
-    
-    var newFrame = document.createElement('iframe');
-    newFrame.setAttribute('id', 'resultFrame');
-    result.appendChild(newFrame);
-    
-    var html = output.html;
-    
-    var script = newFrame.contentWindow.document.createElement('script');
-    script.setAttribute('type', 'text/javascript');
-    script.text = output.script;
-    
-    if (output.library === true) {
-      var uriLib = 'http://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.2/normalize.min.css';
-      var link = newFrame.contentWindow.document.createElement('link');
-      link.setAttribute('rel', 'stylesheet');
-      link.setAttribute('href', uriLib);
-      newFrame.contentWindow.document.head.appendChild(link);
-    }
-    
-    if (output.sass === true) {
-      var scss = output.style;
-      var css = Sass.compile(scss);
-      
-      var style = newFrame.contentWindow.document.createElement('style');
-      style.setAttribute('type', 'text/css');
-      style.innerHTML = css;
-    } else {
-      var style = newFrame.contentWindow.document.createElement('style');
-      style.setAttribute('type', 'text/css');
-      style.innerHTML = output.style;
-    }
-        
-    newFrame.contentWindow.document.body.insertAdjacentHTML('beforeend', html);
-    newFrame.contentWindow.document.head.appendChild(style);
-    newFrame.contentWindow.document.body.appendChild(script);
+		// Rmove iframe if already exists
+		var resultFrame = document.querySelector('#resultFrame');
+		if (resultFrame) { resultFrame.remove() }
+
+		// Set default tab visible if true
+		if (data.view.tab2 === true) { tab2.checked = true }
+		else if (data.view.tab3 === true) { tab3.checked = true }
+		else { tab1.checked = true }
+		
+		// Create new iframe
+		var newFrame = document.createElement('iframe');
+		newFrame.setAttribute('id', 'resultFrame');
+		result.appendChild(newFrame);
+		
+		// Html Input
+		var html = data.html;
+		
+		// Script Input
+		var script = newFrame.contentWindow.document.createElement('script');
+		script.setAttribute('type', 'text/javascript');
+		script.text = data.script;
+		
+		// Import normalize if necessary
+		if (data.library === true) {
+			var uriLib = 'http://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.2/normalize.min.css';
+			var link = newFrame.contentWindow.document.createElement('link');
+			link.setAttribute('rel', 'stylesheet');
+			link.setAttribute('href', uriLib);
+			newFrame.contentWindow.document.head.appendChild(link);
+		}
+		
+		// Compile to SASS or simply load CSS. default is true
+		if (data.sass === true) {
+			var scss = data.style;
+			var css = Sass.compile(scss);
+			
+			var style = newFrame.contentWindow.document.createElement('style');
+			style.setAttribute('type', 'text/css');
+			style.innerHTML = css;
+		} else {
+			var style = newFrame.contentWindow.document.createElement('style');
+			style.setAttribute('type', 'text/css');
+			style.innerHTML = data.style;
+		}
+		
+		// Append HTML, CSS and Script
+		newFrame.contentWindow.document.body.insertAdjacentHTML('beforeend', html);
+		newFrame.contentWindow.document.head.appendChild(style);
+		newFrame.contentWindow.document.body.appendChild(script);
 	}
 	run();
 
@@ -79,13 +100,13 @@
 	 */
 
 	btnRun.addEventListener('click', function() {
-    run();
-    return false;
+		run();
+		return false;
 	});
 
 	btnSave.addEventListener('click', function() {
-    console.log('Save', settings());
-    return false;
+		console.log('Save', settings());
+		return false;
 	});
 
 })();
